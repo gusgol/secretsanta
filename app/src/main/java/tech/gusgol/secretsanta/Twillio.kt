6a0d5e29bca20ae77be2d1f2a2a4425c664d7b77
Gustavo Goldhardt <ggoldhardt@microsoft.com>
2021-11-12 19:40:49 -0300

    Disabled test account

--- app/src/main/java/tech/gusgol/secretsanta/Twillio.kt
+++ app/src/main/java/tech/gusgol/secretsanta/Twillio.kt
@@ -1,6 +1,6 @@
 package tech.gusgol.secretsanta
 
 object Twillio {
-    const val ACCOUNT_SID = "ACf24d94067ba9602cb4723900b743b4d3"
-    const val AUTH_TOKEN = "014b3579be6aa2742027d8ced1b7b3cf"
+    const val ACCOUNT_SID = "ADD_ACCOUNT_HERE"
+    const val AUTH_TOKEN = "ADD_TOKEN_HERE"
 }
